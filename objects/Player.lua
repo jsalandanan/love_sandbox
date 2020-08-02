806dbea8842c334fead72b993a1b5c167aef29b1
jsalandanan <jsalandanan@raybeam.com>
2020-08-02 15:59:41 -0700

    use player attributes for movement bounding

--- objects/Player.lua
+++ objects/Player.lua
@@ -31,11 +31,11 @@ end
 
 function Player:handle_movement(dt)
   if love.keyboard.isDown('d') or love.keyboard.isDown('right') then
-    self.x = math.min(self.x + self.speed * dt, gw-50)
+    self.x = math.min(self.x + self.speed * dt, gw-self.width)
   elseif love.keyboard.isDown('a') or love.keyboard.isDown('left') then
     self.x = math.max(self.x - self.speed * dt, 0)
   elseif love.keyboard.isDown('s') or love.keyboard.isDown('down') then
-    self.y = math.min(self.y + self.speed * dt, gh-50)
+    self.y = math.min(self.y + self.speed * dt, gh-self.height)
   elseif love.keyboard.isDown('w') or love.keyboard.isDown('up') then
     self.y = math.max(self.y - self.speed * dt, 0)
   end
